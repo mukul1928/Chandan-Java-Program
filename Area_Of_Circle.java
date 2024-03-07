c131b4ee89bb2bba0b582c34652dca51db4bfd28
mukul1928 <chandanpandey1928@gmail.com>
2024-03-07 16:48:24 +0530

    Area of circle using math random

---
+++ Area_Of_Circle.java
@@ -0,0 +1,16 @@
+package assignments;
+
+public class Area_Of_Circle 
+{
+	public static void main(String[] args) 
+	
+	{
+		double pi= Math.PI;
+		
+		for(int i=1;i<11;i++)
+		{
+			double r=Math.random();
+		System.out.println(pi*r*r);
+		}
+	}
+}
