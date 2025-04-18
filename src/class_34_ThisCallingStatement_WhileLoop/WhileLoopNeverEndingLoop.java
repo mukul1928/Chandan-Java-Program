b0dd74612d5c8eb4e6f1d1d0f8e342a13b4ab2f9
Chandan Pandey <chandanpandey1928@gmail.com>
2025-04-18 13:30:27 +0530

    Create WhileLoopNeverEndingLoop.java

---
+++ src/class_34_ThisCallingStatement_WhileLoop/WhileLoopNeverEndingLoop.java
@@ -0,0 +1,12 @@
+package class_34_ThisCallingStatement_WhileLoop;
+
+public class WhileLoopNeverEndingLoop {
+	public static void main(String[] args) {
+		int a = 10;
+		while (a > 5) {
+			System.out.println("Chandan Pandey");
+			a++;
+		}
+	}
+
+}
